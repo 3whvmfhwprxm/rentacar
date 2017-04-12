b0d0a9073861ae3920c086e0f1f9afc088237de8
User <User@UserPC>
2017-04-12 14:12:19 +0900

    sql업로드

---
+++ rentACar/src/main/webapp/mj_file/sql문.sql
@@ -0,0 +1,2 @@
+insert into rent_user(user_id,user_pwd,user_name,user_email1,user_email2,user_tel1,user_tel2,user_tel3,user_birth,user_address,user_gender,user_license)
+values('hong3','1234','홍길동','hong','nate.com','010','1111','2222','1980-08-08','경기도','남','1종보통');
