5b37b4f55eae251a56b4b0bb3a58b0825649138f
User <User@UserPC>
2017-04-13 09:10:21 +0900

    1

--- rentACar/src/main/java/com/third/rent/ccaroption/controller/CcarOptionController.java
+++ rentACar/src/main/java/com/third/rent/ccaroption/controller/CcarOptionController.java
@@ -3,7 +3,6 @@ package com.third.rent.ccaroption.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,17 +13,9 @@ public class CcarOptionController {
 	@RequestMapping("/com_manage/regist_option.do")
 	public String optionRegist(){
 		
-		logger.info("옵션등록화면 보여주기");
+		logger.info("옵션등록화면 보여주기11");
 		
 		return "com_manage/regist_option";
 	}
-=======
 
-@Controller
-public class CcarOptionController {
-	private static final Logger logger
-		= LoggerFactory.getLogger(CcarOptionController.class);
-	
-	
->>>>>>> branch 'master' of https://github.com/3whvmfhwprxm/rentacar.git
 }
