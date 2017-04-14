521c326ca56f60cc479adca249ef521ba7c57f2c
User <User@UserPC>
2017-04-14 18:20:47 +0900

    풀푸시

--- rentACar/src/main/java/com/third/rent/ccaroption/controller/CcarOptionController.java
+++ rentACar/src/main/java/com/third/rent/ccaroption/controller/CcarOptionController.java
@@ -2,20 +2,48 @@ package com.third.rent.ccaroption.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.third.rent.ccaroption.model.CcarOptionService;
+import com.third.rent.ccaroption.model.CcarOptionVO;
 
 @Controller
+@RequestMapping("/com_manage")
 public class CcarOptionController {
 	private static final Logger logger =
 			LoggerFactory.getLogger(CcarOptionController.class);
 	
-	@RequestMapping("/com_manage/regist_option.do")
-	public String optionRegist(){
+	@Autowired
+	private CcarOptionService ccarOptionService;
+	
+	@RequestMapping(value="/regist_option.do", method=RequestMethod.GET)
+	public String optionRegist_get(){
 		
 		logger.info("옵션등록화면 보여주기");
 		
 		return "com_manage/regist_option";
 	}
-
+	
+	@RequestMapping(value="/regist_option.do", method=RequestMethod.POST)
+	public String optionRegist_post(@ModelAttribute CcarOptionVO vo,
+			Model model){
+		logger.info("옵션등록처리, 파라미터 vo={}", vo);
+		
+		int cnt = ccarOptionService.insertCcarOption(vo);
+		
+		String msg="", url="/login_company/companyMain.do";
+		if(cnt>0){
+			msg="옵션등록성공";
+		}else{
+			msg="옵션등록실패";
+		}
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+		return "common/message";
+	}
 }
