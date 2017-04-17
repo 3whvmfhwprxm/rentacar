9371779f972803c15ca26360561f05474f120bdc
User <User@UserPC>
2017-04-17 14:08:26 +0900

    asd

--- rentACar/src/main/java/com/third/rent/common/Utility.java
+++ rentACar/src/main/java/com/third/rent/common/Utility.java
@@ -1,6 +1,6 @@
 package com.third.rent.common;
 
 public class Utility {
-	public static final int BLOCKSIZE = 1;
+	public static final int BLOCKSIZE = 3;
 	public static final int RECORDCOUNT_PERPAGE = 2;
 }
