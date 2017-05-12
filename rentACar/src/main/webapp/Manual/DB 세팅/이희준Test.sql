7e039771d8ea966792497f6bf297bb9681f11011
User <User@UserPC>
2017-05-12 14:19:07 +0900

    2차 sql 이희준

--- rentACar/src/main/webapp/Manual/DB 세팅/이희준Test.sql
+++ rentACar/src/main/webapp/Manual/DB 세팅/이희준Test.sql
@@ -11,7 +11,7 @@ to_date('2017-02-07 18:30','yyyy/mm/dd hh24:mi'), '01허1234', '자차보험', '2017-
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
-values('p201000', 'T2000016', '010', '2157', '2630', 'card', 75000, 'paid', '2017-02-02');
+values('p201000', 'T2000016', '010', '2157', '2630', 'card', 75000, 'paid', '2017-02-01');
 
 insert into reserv_user
 values('T2000016', '김진', '010', '5341', '2322', 'kim2@naver.com', '김아빠', '011', '4373', '1112', '801103', '2종보통');
@@ -33,7 +33,7 @@ insert into RESERVATION(
 reserv_num, user_tel1, user_tel2, user_tel3, reserv_start_date, reserv_end_date,
 ccar_car_id, reserv_insurance, reserv_date, user_id)
 values('T2000018', '010','2675','2485', to_date('2017-02-10 09:00','yyyy/mm/dd hh24:mi'), 
-to_date('2017-02-14 11:30','yyyy/mm/dd hh24:mi'), '03허1235', '자차보험', '2017-02-08' ,'kim1');
+to_date('2017-02-14 11:30','yyyy/mm/dd hh24:mi'), '03허1235', '자차보험', '2017-02-09' ,'kim1');
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
@@ -46,11 +46,11 @@ insert into RESERVATION(
 reserv_num, user_tel1, user_tel2, user_tel3, reserv_start_date, reserv_end_date,
 ccar_car_id, reserv_insurance, reserv_date, user_id)
 values('T2000019', '010','4578','3524', to_date('2017-02-18 11:00','yyyy/mm/dd hh24:mi'), 
-to_date('2017-02-20 16:30','yyyy/mm/dd hh24:mi'), '02허5000', '자차보험', '2017-02-02' ,'kim1');
+to_date('2017-02-20 16:30','yyyy/mm/dd hh24:mi'), '02허5000', '자차보험', '2017-02-10' ,'kim1');
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
-values('p201003', 'T2000019', '010', '4578', '3524', 'card', 64000, 'paid', '2017-02-010');
+values('p201003', 'T2000019', '010', '4578', '3524', 'card', 64000, 'paid', '2017-02-10');
 
 insert into reserv_user
 values('T2000019', '김크레', '010', '4466', '5544', 'kim2@naver.com', '임크레', '011', '5353', '4455', '780114', '2종보통');
@@ -59,7 +59,7 @@ insert into RESERVATION(
 reserv_num, user_tel1, user_tel2, user_tel3, reserv_start_date, reserv_end_date,
 ccar_car_id, reserv_insurance, reserv_date, user_id)
 values('T2000020', '010','7822','1358', to_date('2017-02-26 08:30','yyyy/mm/dd hh24:mi'), 
-to_date('2017-02-28 10:30','yyyy/mm/dd hh24:mi'), '01허2222', '자차보험', '2017-02-10' ,'kim1');
+to_date('2017-02-28 10:30','yyyy/mm/dd hh24:mi'), '01허2222', '자차보험', '2017-02-12' ,'kim1');
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
@@ -72,7 +72,7 @@ insert into RESERVATION(
 reserv_num, user_tel1, user_tel2, user_tel3, reserv_start_date, reserv_end_date,
 ccar_car_id, reserv_insurance, reserv_date, user_id)
 values('T2000021', '010','4562','7222', to_date('2017-02-17 17:30','yyyy/mm/dd hh24:mi'), 
-to_date('2017-02-18 10:30','yyyy/mm/dd hh24:mi'), '01허3333', '자차보험', '2017-02-01' ,'kim1');
+to_date('2017-02-18 10:30','yyyy/mm/dd hh24:mi'), '01허3333', '자차보험', '2017-02-10' ,'kim1');
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
@@ -89,7 +89,7 @@ to_date('2017-02-22 13:30','yyyy/mm/dd hh24:mi'), '01허4444', '자차보험', '2017-
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
-values('p201006', 'T2000022', '010', '6436', '6364', 'card', 57800, 'paid', '2017-03-14');
+values('p201006', 'T2000022', '010', '6436', '6364', 'card', 57800, 'paid', '2017-02-14');
 
 insert into reserv_user
 values('T2000022', '김호구', '010', '3522', '2323', 'kim2@naver.com', '박호구', '010', '5533', '7754', '870911', '1종대형');
@@ -115,7 +115,7 @@ to_date('2017-02-27 10:30','yyyy/mm/dd hh24:mi'), '01허6666', '자차보험', '2017-
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
-values('p201008', 'T2000024', '010', '4321', '6646', 'card', 87000, 'paid', '2017-02-12');
+values('p201008', 'T2000024', '010', '4321', '6646', 'card', 87000, 'paid', '2017-02-11');
 
 insert into reserv_user
 values('T2000024', '징크스', '010', '3543', '7723', 'kim2@naver.com', '럭스', '010', '3333', '7554', '710802', '2종보통');
@@ -124,7 +124,7 @@ insert into RESERVATION(
 reserv_num, user_tel1, user_tel2, user_tel3, reserv_start_date, reserv_end_date,
 ccar_car_id, reserv_insurance, reserv_date, user_id)
 values('T2000025', '010','5423','4782', to_date('2017-02-12 10:30','yyyy/mm/dd hh24:mi'), 
-to_date('2017-02-16 10:30','yyyy/mm/dd hh24:mi'), '01허6666', '자차보험', '2017-02-8' ,'kim1');
+to_date('2017-02-16 10:30','yyyy/mm/dd hh24:mi'), '01허6666', '자차보험', '2017-02-08' ,'kim1');
 
 insert into PAYINFO(pay_no, reserv_num, user_tel1, user_tel2, 
 user_tel3, pay_method, pay_money, pay_condition, pay_regdate)
